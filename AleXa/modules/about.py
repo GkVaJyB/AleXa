ab170aaf15a14ce3f51578bb16337d96ced417a4
Asanga Udara <70792747+asangaudara@users.noreply.github.com>
2021-09-17 14:19:19 +0530

    Add files via upload

---
+++ AleXa/modules/about.py
@@ -0,0 +1,16 @@
+__help__ = """
+** ✨ AleXa - A Powerful Telegram Group Manager ✨ **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+
+
+Developer [Asanga Udara](https://t.me/asanga_udara)
+"""
+__mod_name__ = "About ⚡"
