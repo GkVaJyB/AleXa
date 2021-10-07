d0a7e69ba61079621c97270fa319c96c6d4c2cf6
GkVaJyB <89921421+GkVaJyB@users.noreply.github.com>
2021-10-07 20:40:14 +0530

    Update video.py

--- AleXa/modules/video.py
+++ AleXa/modules/video.py
@@ -31,7 +31,7 @@ async def ytmusic(client, message: Message):
         )
         return
     global dl_limit
-    if dl_limit >= 4:
+    if dl_limit >= 99:
         await message.reply_text(
             "Daisy s server busy due to too many downloads, try again after sometime."
         )
