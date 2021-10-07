222f1ce7a0e3683f09bf862bf8fdc6b66a8dead8
GkVaJyB <89921421+GkVaJyB@users.noreply.github.com>
2021-10-07 22:30:55 +0530

    Update video.py

--- AleXa/modules/video.py
+++ AleXa/modules/video.py
@@ -14,6 +14,7 @@ import requests
 import wget
 import youtube_dl
 from pyrogram import Client, filters
+from AleXa import LOGGER, pbot as Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
