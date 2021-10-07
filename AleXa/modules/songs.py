856748a959e840dbc5a919e975665f315225ea88
GkVaJyB <89921421+GkVaJyB@users.noreply.github.com>
2021-10-07 22:58:41 +0530

    Update songs.py

--- AleXa/modules/songs.py
+++ AleXa/modules/songs.py
@@ -1,93 +1,99 @@
+
 import os
-
-import aiohttp
-from pyrogram import filters
-from pytube import YouTube
-from youtubesearchpython import VideosSearch
-
-from AleXa import LOGGER, pbot
-from AleXa.utils.uut import get_arg
-from AleXa.sql.chat_sql import add_chat_to_db
-
-
-def yt_search(song):
-    videosSearch = VideosSearch(song, limit=1)
-    result = videosSearch.result()
-    if not result:
-        return False
-    else:
-        video_id = result["result"][0]["id"]
-        url = f"https://youtu.be/{video_id}"
-        return url
+import time
+import ffmpeg
+import asyncio
+import requests
+import youtube_dl
+from config import Config
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from youtube_search import YoutubeSearch
 
 
-class AioHttp:
-    @staticmethod
-    async def get_json(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.json()
+## Extra Fns -------------------------------
 
-    @staticmethod
-    async def get_text(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.text()
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
-    @staticmethod
-    async def get_raw(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.read()
 
-thumb_name = 'photo_2021-08-29_15-16-15.jpg'
-@pbot.on_message(filters.command("song"))
-async def song(client, message):
-    chat_id1 = message.chat.id
-    user_id1 = message.from_user["id"]
-    name1 = message.from_user["first_name"]
-    add_chat_to_db(str(chat_id1))
-    message.chat.id
-    user_id = message.from_user["id"]
-    args = get_arg(message) + " " + "song"
-    if args.startswith(" "):
-        await message.reply("Enter a song name. Check /help")
-        return ""
-    status = await message.reply("**Downloading Song** 😊")
-    video_link = yt_search(args)
-    if not video_link:
-        await status.edit("**Song not found.** 🤔")
-        return ""
-    yt = YouTube(video_link)
-    audio = yt.streams.filter(only_audio=True).first()
+## Commands --------------------------------
+
+@bot.on_message(filters.command(['sg']))
+async def song(_, message: Message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    k=await message.reply_text("🔍 **Searching Song...**")
+    ydl_opts = {
+        "format": "bestaudio[ext=m4a]",
+        "geo-bypass": True,
+        "nocheckcertificate": True,
+        "outtmpl": "downloads/%(id)s.%(ext)s",
+        }
     try:
-        download = audio.download(filename=f"{str(yt.title)}")
-    except Exception as ex:
-        await status.edit("Failed to download song")
-        LOGGER.error(ex)
-        return ""
-    os.rename(download, f"{str(yt.title)}.mp3")
-    await pbot.send_chat_action(message.chat.id, "upload_audio")
-    await pbot.send_audio(
-        chat_id=message.chat.id,
-        audio=f"{str(yt.title)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        thumb=thumb_name,
-        reply_to_message_id=message.message_id,
-       )
-    await pbot.send_audio(
-        chat_id=-1001486361068,
-        audio=f"{str(yt.title)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        thumb=thumb_name,
-       )
-    await status.delete()
-    os.remove(f"{str(yt.title)}.mp3")
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count > 0:
+                await time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+            views = results[0]["views"]
 
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+
+            performer = f"[ꜱᴀꜰᴏɴᴇ ᴍᴜꜱɪᴄ]" 
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            await k.edit('❌ **Found Literary Noting! \nPlease Try Another Song or Use Correct Spelling.**')
+            return
+    except Exception as e:
+        await k.edit(
+            "❗ **Enter An Song Name!** \nFor Example: `/song Alone Marshmellow`"
+        )
+        print(str(e))
+        return
+    await k.edit("📥 **Downloading Song...**")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        cap = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Requested By:</b> {message.from_user.mention()} \n📤 <b>Uploaded By: <a href="https://t.me/AsmSafone">🇧🇩 Ｓ１ ＢＯＴＳ</a></b>'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        await k.edit("📤 **Uploading Song...**")
+        await message.reply_audio(audio_file, caption=cap, parse_mode='HTML', title=title, duration=dur, performer=performer, thumb=thumb_name)
+        await k.delete()
+        await mp.delete(message)
+    except Exception as e:
+        await k.edit(f'❌ **An Error Occured!** \n\nError:- {e}')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
 
 __help__ = """
 *New Update Songs Saveing REAL NAME*
